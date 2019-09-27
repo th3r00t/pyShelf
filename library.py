5d26a7d49f096daba44c4531bd58df75d71f1eb4
Mike <admin@mylocaltech.co>
2019-09-26 22:06:38 -0400

    Implement directory recursion, and type filtering

--- library.py
+++ library.py
@@ -9,6 +9,7 @@ config = Config()
 class Catalogue:
     """Decodes and stores book information"""
     def __init__(self):
+        self.file_list = []
         with open(config.book_shelf, 'r') as f:
             try:
                 self.catalogue = json.load(f)
@@ -17,24 +18,26 @@ class Catalogue:
                 with open(config.book_shelf, 'w') as f:
                     json.dump(self.filter_books(), f)
 
-    def scan_folder(self):
-        file_list = []
-        for f in os.listdir(config.book_path):
-            file_list.append(f)
-        return file_list
+    def scan_folder(self, folder=config.book_path):
+        for f in os.listdir(folder):
+            _path = os.path.abspath(folder+'/'+f)
+            #_path = os.path.abspath('.')+'/'+folder+f+'/'
+            _is_dir = os.path.isdir(_path.strip()+'/')
+            if _is_dir:
+                self.file_list.append(self.scan_folder(_path))
+            self.file_list.append(_path)
 
     def filter_books(self):
         scan = self.scan_folder()
-        breakpoint()
         regx = re.compile(r"\.epub")
-        self.books = list(filter(regx.search, scan))
-        return self.books
+        self.books = list(filter(regx.search, filter(None, self.file_list)))
+        with open(config.book_shelf, 'w') as f:
+                json.dump(self.books, f)
 
     def compare_shelf_current(self):
         try:
             self.books
         except Exception:
             self.filter_books()
-        breakpoint()
         unique = set(self.books) - set(self.catalogue)
         return unique
--- tests/test_library.py
+++ tests/test_library.py
@@ -7,8 +7,8 @@ class Testing(unittest.TestCase):
     def test_libray_catalogue(self):
         self.assertIsNotNone(Catalogue())
 
-    def test_library_catalogue_scan_folder(self):
-        self.assertIsInstance(Catalogue().scan_folder(), object)
+    def test_library_catalogue_filter_books(self):
+        self.assertIsNotNone(Catalogue().filter_books())
 
     def test_library_catalogue_compare_shelf_current(self):
         self.assertIsInstance(Catalogue().compare_shelf_current(), Catalogue)
