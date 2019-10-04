d7f0e2dab5bd8898286fecb33063bdbb6197f3b4
Mike <admin@mylocaltech.co>
2019-10-04 17:15:14 -0400

    Search DuckDuckGo for a cover image

---
+++ lib/api_hooks.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import sys
+import requests
+sys.path.insert(1, 'lib/')
+
+
+class DuckDuckGo:
+    """duckduckgo related searching"""
+    def __init__(self):
+        self.url = "https://api.duckduckgo.com/?q="
+
+    def image_result(self, query):
+        """
+        Returns json containing url to image
+        :param _key: &t=h_&iar=images&iax=images&ia=images&format=json&pretty=1
+        """
+        _key = '&t=h_&iar=images&iax=images&ia=images&format=json&pretty=1'
+        try: query = query.string
+        except AttributeError: query = query
+        search_result = requests.get(self.url+query+_key)
+        if search_result.status_code == 200 and search_result.json()['Image'] != '':
+            image = requests.get(search_result.json()['Image'], stream=True)
+            image.raw.decode_content = True
+            return image.raw
+        else: return False
