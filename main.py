f0b461ab0b98ebbaf211600665e6c566836c8d4f
Mike <admin@mylocaltech.co>
2019-10-06 16:51:53 -0400

    Removed whitespace & Added TODO list

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ from library import Catalogue
 config = Config() # Get configuration settings
 InitFiles(config.file_array) # Initialize file system
 Catalogue = Catalogue() # Open the Catalogue
-
 # This only needs to be run on first run, & when new books are added
 Epub().import_books() # Filter Your books
+# TODO Implement file tracking system to avoid processing already tracked books
+# TODO Figure out a system to get books page count
+# TODO Update testing
+# TODO Update Documentation
+# TODO Requirements.txt
