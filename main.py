7dd0fe00e0e00eeb2dccff3f5b4d6848210a14b8
Mike <admin@mylocaltech.co>
2019-10-06 16:42:05 -0400

    Refactored for cleaner code

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python
-# import zipfile as Zip
 import sys
 from PIL import Image
 sys.path.insert(1, 'lib/')
 from pyShelf import InitFiles, Epub
 from config import Config
 from library import Catalogue
-# Get configuration settings
-config = Config()
-# Initialize file system
-InitFiles(config.file_array)
-# Open the Catalogue
-Catalogue = Catalogue()
-# Filter Your books
+
+config = Config() # Get configuration settings
+InitFiles(config.file_array) # Initialize file system
+Catalogue = Catalogue() # Open the Catalogue
+
 # This only needs to be run on first run, & when new books are added
-Epub().import_books()
+Epub().import_books() # Filter Your books
