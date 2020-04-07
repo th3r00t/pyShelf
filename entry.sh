d9ba44e3170d3bb49b326e389266ba3714553b0d
Raelon Masters <admin@mylocaltech.co>
2020-04-07 15:03:55 -0400

    Modified entry.sh to pull changes from git

--- entry.sh
+++ entry.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo -u postgres pg_ctl -D /var/lib/postgres/data -l /srv/logs/pgsql.log start
+git pull
 sudo -u postgres psql -f create_db.sql
 rm /srv/http/entry.sh
 echo "sudo -u postgres pg_ctl -D /var/lib/postgres/data -l /srv/logs/lgsql.log start" > /srv/http/entry.sh
