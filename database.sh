feabc373b8145e6de21903bd3f71e7e72cb1ff6a
Raelon Masters <admin@mylocaltech.co>
2020-04-07 16:29:27 -0400

    Adjusted entrypoint ops

--- database.sh
+++ database.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-sudo -u postgres pg_ctl -D /var/lib/postgres/data -l /srv/logs/pgsql.log start
-sudo -u postgres psql -f create_db.sql
-rm /srv/http/database.sh
-echo "sudo -u postgres pg_ctl -D /var/lib/postgres/data -l /srv/logs/lgsql.log start" > /srv/http/database.sh
-chmod +x /srv/http/database.sh
+sudo -u postgres pg_ctl -D /var/lib/postgres/data -l /srv/logs/pgsql.log start &&
+sudo -u postgres psql -f create_db.sql &&
+rm /srv/http/database.sh &&
+echo "sudo -u postgres pg_ctl -D /var/lib/postgres/data -l /srv/logs/lgsql.log start" > /srv/http/database.sh &&
+chmod +x /srv/http/database.sh &&
+cd src && \
+    python manage.py makemigrations && \
+    python manage.py makemigrations interface && \
+    python manage.py migrate && \
+    python manage.py migrate interface && \
+cd .. &&
