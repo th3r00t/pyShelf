1aeb2fbea6fa81962005f93a0ab90ccf1a6c0e44
Raelon Masters <admin@mylocaltech.co>
2020-04-07 15:10:52 -0400

    Restructure entrypoint ops

--- entry.sh
+++ entry.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-sudo -u postgres pg_ctl -D /var/lib/postgres/data -l /srv/logs/pgsql.log start
 git pull
-sudo -u postgres psql -f create_db.sql
-rm /srv/http/entry.sh
-echo "sudo -u postgres pg_ctl -D /var/lib/postgres/data -l /srv/logs/lgsql.log start" > /srv/http/entry.sh
-chmod +x /srv/http/entry.sh
+sh -c database.sh
