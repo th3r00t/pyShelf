a2cd08efe7e92595ee570aea565b702d78ab2009
MartenBE <martijn.saelens@protonmail.com>
2020-08-09 23:57:44 +0200

    Fixed small error in Config

--- src/backend/lib/config.py
+++ src/backend/lib/config.py
@@ -37,7 +37,7 @@ class Config:
         self.db_user = _data["USER"]
         self.db_pass = _data["PASSWORD"]
         self.SECRET = _data["SECRET"]
-        self.debug_build_mode = (_data["SECRET"].casefold() == "debug")
+        self.debug_build_mode = (_data["BUILD_MODE"].casefold() == "debug")
 
     def open_file(self, _cp):
         """
