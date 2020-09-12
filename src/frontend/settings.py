d4a7fb0b7d08bd7129fd047b4f25ed2d6e8cb088
th3r00t <admin@mylt.dev>
2020-09-11 21:03:11 -0400

    Added whitenoise to serve static files

--- src/frontend/settings.py
+++ src/frontend/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = "interface.User"
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
