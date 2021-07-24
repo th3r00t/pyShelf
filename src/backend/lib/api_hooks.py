efa5c057e4b7ac274770374ead13da59b2ecba1a
th3r00t <myoung0083@gmail.com>
2021-07-24 02:02:40 -0400

    Begin debugging search query

--- src/backend/lib/api_hooks.py
+++ src/backend/lib/api_hooks.py
@@ -16,6 +16,7 @@ class DuckDuckGo:
         :param _key: &t=h_&iar=images&iax=images&ia=images&format=json&pretty=1
         """
         _key = "&t=h_&iar=images&iax=images&ia=images&format=json&pretty=1"
+        breakpoint()
         try:
             query = query.string
         except AttributeError:
