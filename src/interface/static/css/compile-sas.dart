6fb757cc31338b5d410523e0004adaf4da0d88cf
th3r00t <admin@mylt.dev>
2022-11-13 01:19:44 -0500

    Removed Django Framework

--- src/interface/static/css/compile-sas.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:io';
-import 'package:sass/sass.dart' as sass;
-
-void main(List<String> arguments) {
-  var result = sass.compile(arguments[0]);
-  new File(arguments[1]).writeAsStringSync(result);
-}
