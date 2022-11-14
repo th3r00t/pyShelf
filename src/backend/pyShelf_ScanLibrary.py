47a07cdc2590fa11f300ce6ef43880349a9271a2
th3r00t <admin@mylt.dev>
2022-11-14 00:05:34 -0500

    dropped un unsed error assignment

--- src/backend/pyShelf_ScanLibrary.py
+++ src/backend/pyShelf_ScanLibrary.py
@@ -16,8 +16,10 @@ def execute_scan(root, **kwargs):
     :param root: Project root. Required to properly execute program. Sends to configuration.
     """
     _t1 = time.time()
-    try: config = kwargs["config"];
-    except KeyError as e: config = Config(root)  # Get configuration settings
+    try:
+        config = kwargs["config"]
+    except KeyError:
+        config = Config(root)  # Get configuration settings
     InitFiles(config.file_array)  # Initialize file system
     catalogue = Catalogue(config)  # Open the Catalogue
     catalogue.import_books()
