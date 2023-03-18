38cac3c1053b588186d7d5d256469676705eb9fb
th3r00t <admin@mylt.dev>
2023-03-18 12:27:05 -0400

    Next scaffolding

---
+++ src/client/src/types/index.tsx
@@ -0,0 +1,18 @@
+// Path: types/index.tsx
+
+export interface IBook {
+  book_id: string
+  title: string
+  author: string
+  categories: string
+  cover: string
+  pages: string
+  progress: string
+  file_name: string
+  description: string
+  date: string
+  rights: string
+  tags: string
+  identifier: string
+  publisher: string
+}
---
+++ src/client/theme.tsx
@@ -0,0 +1,20 @@
+import { createTheme } from @material-ui/core/styles;
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: '#f44336',
+    },
+    background: {
+      default: '#fff',
+    },
+  },
+});
+
+export { theme };
