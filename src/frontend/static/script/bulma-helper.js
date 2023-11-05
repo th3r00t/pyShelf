d1e095b871c139a0b10e54dbd2e43a3b1b873135
th3r00t <tty0@th3r00t.dev>
2023-11-04 23:42:34 -0400

    Refactored into pyShelf.ts.

--- src/frontend/static/script/bulma-helper.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  $(".navbar-burger").click(function(){
-    $(".navbar-burger").toggleClass("is-active");
-    $(".navbar-menu").toggleClass("is-active");
-  });
-});
