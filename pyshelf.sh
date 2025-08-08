f59d6530a9c7dfdf18ad56b501078369165f0e1a
th3r00t <tty303@proton.me>
2025-08-08 21:11:51 +0000

    Created install.sh and pyshelf.sh
    Also refactored methods to work as a zipapp

--- pyshelf.sh
+++ pyshelf.sh
@@ -2,15 +2,15 @@
 # This script is used to run the pyshelf application.
 # It sets up the environment and runs the main script.
 # first we need to activate the virtual environment
-if [ -d ".venv" ]; then
-	source .venv/bin/activate
+if [ -d "/etc/pyShelf/.venv" ]; then
+	source /etc/pyShelf/.venv/bin/activate
 else
 	echo "Virtual environment not found. Please create it first."
 	exit 1
 fi
 # then we need to run the main script
 if [ -f "release/pyshelf" ]; then
-	./release/pyshelf	
+	sudo -u pyshelf /etc/pyShelf/release/pyshelf	
 else
 	echo "Main script not found. Please check the directory."
 	exit 1
